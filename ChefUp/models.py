6ac566fe16dd9301e53dfe7c025f934a5c63e37e
Anh <anhduongnguyen51015@gmail.com>
2025-04-23 23:03:00 +1000

    Setting Up project

---
+++ ChefUp/models.py
@@ -0,0 +1,50 @@
+from . import db
+from datetime import datetime
+from flask_login import UserMixin
+
+''' ORIGINAL CODE FROM TEMPLATE 
+class User(db.Model, UserMixin):
+    pass  # Placeholder for now
+
+class Event(db.Model):
+    pass  # Placeholder for now
+
+class Comment(db.Model):
+    pass  # Placeholder for now
+
+class Order(db.Model):
+    pass  # Placeholder for now
+
+'''
+
+# Filler code at the moment as database isnt set up yet
+    
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    username = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    location = db.Column(db.String(100), nullable=False)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+    quantity = db.Column(db.Integer, nullable=False)
+    order_date = db.Column(db.DateTime, default=datetime)
+
+    
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from ChefUp import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, port=5001)
